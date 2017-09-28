9b7b04fa848025d23eb3c306dbc0890a69398ada
Young <Young@JIAYANGdeMBP.lan>
2017-09-28 15:36:28 +1000

    work in progress

--- designs.js
+++ designs.js
@@ -3,8 +3,35 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
 
-// Your code goes here!
+
+let mk = function makeGrid(event) {
+
+	event.preventDefault();
+	let rowLength = $('#input_width').val()
+	let columnLength = $('#input_height').val()
+
+	for (var rows = 0; rows < rowLength; rows++) {
+			$("body").append("<br>");
+	        for (var columns = 0; columns < columnLength; columns++) {
+	            $("body").append("<div class='grid'></div>");
+	        };
+	    };
+	    $(".grid").width(100);
+	    $(".grid").height(100);
 
 }
+
+$('#create').on('click', mk);
+
+
+
+let cc = function changeColor(){
+
+	$('body').css('color', 'green');
+	let gridColor = $('#colorPicker').val();
+	$(this).css('background-color', gridColor);
+}
+
+$('body').on('click','.grid', cc);
+
