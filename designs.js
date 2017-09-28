b4377cb8a5ae96d3c7c3dcbfbb32f59d8c2960c1
Young <Young@JIAYANGdeMBP.lan>
2017-09-28 16:17:25 +1000

    finish

--- designs.js
+++ designs.js
@@ -8,27 +8,25 @@
 let mk = function makeGrid(event) {
 
 	event.preventDefault();
+	$('table').empty();
 	let rowLength = $('#input_width').val()
 	let columnLength = $('#input_height').val()
 
-	for (var rows = 0; rows < rowLength; rows++) {
-			$("body").append("<br>");
-	        for (var columns = 0; columns < columnLength; columns++) {
-	            $("body").append("<div class='grid'></div>");
+	for (var columns = 0; columns < columnLength; columns++) {
+			$("table").append("<br>");
+	        for (var rows = 0; rows < rowLength; rows++) {
+	            $("table").append("<div class='grid'></div>");
 	        };
 	    };
-	    $(".grid").width(100);
-	    $(".grid").height(100);
+	    $(".grid").width("20px");
+	    $(".grid").height("20px");
 
 }
 
 $('#create').on('click', mk);
 
-
-
 let cc = function changeColor(){
 
-	$('body').css('color', 'green');
 	let gridColor = $('#colorPicker').val();
 	$(this).css('background-color', gridColor);
 }
